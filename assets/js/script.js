cf3323990b7f1f5b9c6a0bd63e480ad3e3c9951d
mzelenevskaya <mzelenevskaya@gmail.com>
2022-06-12 20:42:34 +0300

    feat: change html and css layouts for AXIT-landing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,77 @@
 // console.log();
 
+// function 
+(function () {
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add('header_active');
+        } else {
+            header.classList.remove('header_active');
+        }
+    };
+}());
+
+//burger header
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const menuCloseItem = document.querySelector('.header__nav-close');
+    const menulinks = document.querySelectorAll('.header__menu-link')
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__nav_active');
+   });
+   menuCloseItem.addEventListener('click', () => {
+    menu.classList.remove('header__nav_active');
+   });
+   if (window.innerWidth <= 767) {
+       for (let i = 0; i < menulinks.length; i += 1) {
+        menulinks[i].addEventListener('click', () => {
+            menu.classList.remove('header__nav_active');
+        });
+    }
+   }
+}());
+
+//scroll to anchors
+(function () {
+
+    const smoothScroll = function (targetEl, duration) {
+        const headerElHeight = document.querySelector('.header').clientHeight;
+        let target = document.querySelector(targetEl);
+        let targetPossition = target.getBoundingClientRect().top - headerElHeight;
+        let startPossition = window.pageYOffset;
+        let startTime = null;
+
+        const ease = function(t,b,c,d) {
+            t /=d /2;
+            if (t < 1) return c /2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t -2) - 1) + b;            
+        };
+
+        const animation = function(currentTime) {
+            if (startTime == null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPossition, targetPossition, duration);
+            window.scrollTo(0,run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+    };
+
+    const scrollTo = function () {
+        const links = document.querySelectorAll('.js-scroll');
+        links.forEach(each => {
+            each.addEventListener('click', function () {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+    scrollTo();
+}());
+
 // tab
 function openCity(evt, target) {
     var i, tabs__card, tabs__item;
